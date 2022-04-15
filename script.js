9ae92ee22040895a4108c730208e953d1eca5599
jorti171 <103783842+jorti171@users.noreply.github.com>
2022-04-15 18:56:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ $(document).ready(function() {
     }
   
      Highcharts.setOptions({
-       colors: ['#DA7345' , '6C91C2']
+       colors: ['#DA7635' , '6C91C2']
                });
     // Now create the chart
     Highcharts.chart('container', {
